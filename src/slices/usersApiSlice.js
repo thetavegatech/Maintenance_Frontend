3e8949f42016c2fc2050097b1c9076cebc07693c
thetavegatech <thetavegatech@gmail.com>
2023-12-15 12:52:00 +0530

    fifth commit

--- src/slices/usersApiSlice.js
+++ src/slices/usersApiSlice.js
@@ -1,18 +1,18 @@
 import { apiSlice } from './apiSlice'
-const USERS_URL = 'http://192.168.29.93:5000/api/users'
+const USERS_URL = 'https://mms-frontend.onrender.com/api/users'
 
 export const usersApiSlice = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
     login: builder.mutation({
       query: (data) => ({
-        url: `${'http://192.168.29.93:5000/api/users'}/auth`,
+        url: `${'https://mms-frontend.onrender.com/api/users'}/auth`,
         method: 'POST',
         body: data,
       }),
     }),
     logout: builder.mutation({
       query: () => ({
-        url: `${'http://192.168.29.93:5000/api/users'}/logout`,
+        url: `${'https://mms-frontend.onrender.com/api/users'}/logout`,
         method: 'POST',
       }),
     }),
--- src/views/pages/register/Register.js
+++ src/views/pages/register/Register.js
@@ -37,7 +37,7 @@ const Register = () => {
     e.preventDefault()
 
     try {
-      const response = await axios.post('http://192.168.29.93:5000/api/users', {
+      const response = await axios.post('https://mms-frontend.onrender.com/api/users', {
         name: formData.username,
         email: formData.email,
         password: formData.password,
